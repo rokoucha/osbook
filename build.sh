280d785cec853a441a2650c787e2e97cd90eb874
Rokoucha <git@rokoucha.net>
2024-02-06 21:12:04 +0900

    day04a

--- build.sh
+++ build.sh
@@ -15,8 +15,5 @@ cd ..
 # shellcheck disable=SC1091
 source osbook/devenv/buildenv.sh
 cd kernel
-# shellcheck disable=SC2086,SC2154,SC2250
-clang++ $CPPFLAGS -O2 --target=x86_64-elf -fno-exceptions -ffreestanding  -c main.cpp
-# shellcheck disable=SC2086,SC2154,SC2250
-ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base 0x100000 --static -o kernel.elf main.o
+make
 cd ..
